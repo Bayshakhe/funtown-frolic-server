c5d3933edd7196585fe7415255edc1a6ff49920a
Bayshakhe Pramanik <bayshakhepramanik@gmail.com>
2023-06-10 02:05:23 +0600

    generate the jwt token

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const jwt = require('jsonwebtoken')
 const app = express();
 const port = process.env.PORT || 5000;
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
+  app.use(cors(corsOptions))
 app.use(express.json());
-app.use(cors())
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send("It's Funtown")
 })
 
@@ -19,64 +24,87 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 const usersCollections = client.db('funtownFrolicDb').collection('users')
 const classesCollections = client.db('funtownFrolicDb').collection('classes')
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
 
-app.post('/users', async(req,res) => {
-    const user = req.body;
-    console.log(user)
-    // const user = {email: email}
-    const result = await usersCollections.insertOne(user)
-    res.send(result)
-})
+        // generate jwt token
+        app.post('/jwt', (req,res) => {
+            const email = req.body;
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+            res.send({token})
+        })
 
-app.get('/classes', async(req,res) => {
-    const query ={
-        status: 'approved'
+        // user related apis
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await usersCollections.findOne(query)
+            if (existingUser) {
+                return res.send({ message: 'User already exist' })
+            }
+            // const user = {email: email}
+            const result = await usersCollections.insertOne(user)
+            res.send(result)
+        })
+
+        // class related apis
+        app.get('/classes', async (req, res) => {
+            const query = {
+                status: 'approved'
+            }
+            const options = {
+                sort: { 'enrolled': -1 }
+            }
+            const result = await classesCollections.find(query, options).toArray();
+            res.send(result)
+        })
+
+        // instructor related apis
+        app.get('/instructors', async (req, res) => {
+            const query = {}
+            const options = {
+                sort: { 'enrolled': -1 }
+            }
+            const result = await classesCollections.find(query, options).toArray();
+            res.send(result)
+        })
+        app.get('/instructor', async(req,res) => {
+            const email = req.query.email;
+            const query = {
+                instructorEmail: email
+            }
+            const result = await classesCollections.find(query).toArray()
+            res.send(result)
+        })
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
-    const options = {
-        sort:{'price': -1}
-    }
-    const result = await classesCollections.find(query, options).toArray();
-    res.send(result)
-})
-app.get('/instructors', async(req,res) => {
-    const query = {}
-    const options = {
-        sort:{'enrolled': -1}
-    }
-    const result = await classesCollections.find(query, options).toArray();
-    res.send(result)
-})
-
-
-
-
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
 }
 run().catch(console.dir);
 
 
 
-app.listen(port, (req,res) => {
+app.listen(port, (req, res) => {
     console.log('Funtown is running port', port)
 })
